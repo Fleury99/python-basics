b656e259a7413786a9fdd34f1ae87de2ce8cd471
Fleury99 <kpemissiefleury@gmail.com>
2026-05-21 23:15:37 +0000

    add: sum two numbers exercice

---
+++ basics/sum_two_numbers.py
@@ -0,0 +1,7 @@
+#Exo 3 - Ask for two numbers and then display their sum.
+
+number_one = int(input("Enter first number:>>> "))
+number_two = int(input("Enter second number:>>> "))
+
+sum = number_one + number_two
+print(f"sum: {sum}")
