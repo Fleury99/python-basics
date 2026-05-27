bf2dec5b55757e7129922bca9d18a22511c0d3a2
Fleury99 <kpemissiefleury@gmail.com>
2026-05-27 01:40:43 +0000

    add: even odd check exercice

---
+++ basics/even_odd_check.py
@@ -0,0 +1,7 @@
+#Exo 2 - Chek if the user input number is pair or odd
+number = int(input("Enter the number please: "))
+
+if number % 2 == 0 :
+    print(f"the number {number} is pair")
+else :
+    print(f"the number {number} is odd")
