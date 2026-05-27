79b6fda5e49ea6aa6a7a1e98c054964312aa6b50
Fleury99 <kpemissiefleury@gmail.com>
2026-05-27 01:41:54 +0000

    add: grading system exercice

---
+++ basics/grading_system.py
@@ -0,0 +1,15 @@
+#Exo 3 - grading system
+number = int(input("Enter the number please: "))
+
+if number >= 18 :
+    print("Excellent")
+elif number >= 16 :
+    print("Alright")
+elif number >= 14 :
+    print("Good")
+elif number >= 12 :
+    print("Fairly Good")
+elif number >= 10 :
+    print("Average")
+else :
+    print("Failure")
