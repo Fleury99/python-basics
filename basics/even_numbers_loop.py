525d2b3dec85dd12117d81525624d75271697496
Fleury99 <kpemissiefleury@gmail.com>
2026-05-28 23:57:09 +0000

    add: for loop exercises

---
+++ basics/even_numbers_loop.py
@@ -0,0 +1,11 @@
+#Exo 1 - Display only even numbers.
+for i in range(2, 102, 2):
+    print(i)
+
+#Another way of writing it 
+for i in range(1, 100, 2):
+    print(i + 1)
+
+# Affiche les nombres pairs de 0 à 10 inclus
+for i in range(0, 11, 2):
+    print(i)
