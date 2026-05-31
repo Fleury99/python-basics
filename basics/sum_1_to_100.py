c7fc67546002611d7b308da3299028e46b6343ea
Fleury99 <kpemissiefleury@gmail.com>
2026-05-31 02:25:58 +0000

    add: while loop exercises

---
+++ basics/sum_1_to_100.py
@@ -0,0 +1,10 @@
+# Initializing variables
+number = 1
+total_sum = 0
+
+# The loop adds every number up to 100
+while number <= 100:
+    total_sum += number  # Adds the current number to the total sum
+    number += 1          # Moves to the next number
+
+print("The sum of numbers from 1 to 100 is:", total_sum)
---
+++ basics/while_counter.py
@@ -0,0 +1,9 @@
+# Initializing the counter
+counter = 1
+
+# The loop runs as long as the counter is less than or equal to 10
+while counter <= 10:
+    print(counter)
+    counter += 1  # Incrementation (important to avoid an infinite loop)
+
+  
