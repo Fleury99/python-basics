2044e284eb8ff76886687f153e49a19cfc973a17
Fleury99 <kpemissiefleury@gmail.com>
2026-06-01 23:59:55 +0000

    add: basic functions exercises

---
+++ basics/addition_function.py
@@ -0,0 +1,17 @@
+def addition(a, b):
+    return a + b
+    #print( a + b)
+
+
+print(addition(1, 2))
+print(addition(5, 17))
+print(addition(42, 78))
+print(addition(10, 90))
+#addition(1, 2)
+#addition(5, 17)
+#addition(42, 78)
+#addition(10, 90)
+
+result = addition(17, 87)
+
+print(f"The result of this addition is {result}")
---
+++ basics/hello_function.py
@@ -0,0 +1,7 @@
+def hello(name):
+    print(f"Hello {name} nice too meet you!!!!")
+
+
+
+hello("Fleury")
+hello("Arthur")
